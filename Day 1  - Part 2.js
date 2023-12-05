db57e358c9852490cfcd12d52735e3e3bc5e8598
billikw <66752314+billikw@users.noreply.github.com>
2023-12-05 20:19:37 +0000

    Update Day 1  - Part 2.js

--- Day 1  - Part 2.js
+++ Day 1  - Part 2.js
@@ -3,7 +3,17 @@
 const formattedInput = input.split("\n");
 
 const calibratedValues = formattedInput.map((input) => {
-		const conversion = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"};
+		const conversion = {
+			"one": "1", 
+			"two": "2",
+			"three": "3",
+			"four": "4",
+			"five": "5",
+			"six": "6",
+			"seven": "7",
+			"eight": "8",
+			"nine": "9"
+		};
 		let tempString = "";
 		let lastValidString = "";
 		const result = []
